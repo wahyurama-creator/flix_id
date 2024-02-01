29265aa5f0e2e1ca7b2759dcd8b15a82209c59b4
wahyu rama <wahyuramadhon32@gmail.com>
2024-02-01 13:12:41 +0700

    Config colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flix_id/firebase_options.dart';
+import 'package:flix_id/presentation/misc/constants.dart';
 import 'package:flix_id/presentation/providers/router/router_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   // Load .env file
@@ -23,6 +25,24 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
+      theme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: saffronColor,
+          background: backgroundColor,
+          brightness: Brightness.dark,
+        ),
+        textTheme: TextTheme(
+          bodyMedium: GoogleFonts.poppins(
+            color: ghostWhiteColor,
+          ),
+          bodyLarge: GoogleFonts.poppins(
+            color: ghostWhiteColor,
+          ),
+          labelLarge: GoogleFonts.poppins(
+            color: ghostWhiteColor,
+          ),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       routeInformationParser: ref.watch(routerProvider).routeInformationParser,
       routeInformationProvider:
---
+++ lib/presentation/misc/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = Color(0xFF1F1D2B);
+const Color ghostWhiteColor = Color(0xFFF6F8FF);
+const Color saffronColor = Color(0xFFF4C95D);
